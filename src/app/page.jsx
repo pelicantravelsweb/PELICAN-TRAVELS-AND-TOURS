5792cbc4ecc448f4609a35d70da14ac199327809
Yohan Adikari <amkyadikari@gmail.com>
2026-04-07 20:24:36 +0530

    Update Next.js version and fix navigation links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -138,7 +138,7 @@ const handleInquirySubmit = async (e) => {
         <Link href="/"><h1 className={styles_1.heading}>PELICAN TOURS</h1></Link>
         <ul className={styles_1.navigation_ul}>
             <li className={styles_1.navigation_desktop}><Link href="/"><h2 id={styles_1.active}>HOME</h2></Link></li>
-            <li className={styles_1.navigation_desktop}><Link href="https://pelican-travels.web.app/tour-packages"><h2>PACKAGES</h2></Link></li>
+            <li className={styles_1.navigation_desktop}><Link href="/tour-packages"><h2>PACKAGES</h2></Link></li>
             <li className={styles_1.navigation_desktop}><Link href="/services"><h2>SERVICES</h2></Link></li>
             <li className={styles_1.navigation_desktop}><Link href="/destinations"><h2>DESTINATIONS</h2></Link></li>
             <li className={styles_1.navigation_desktop}><Link href="/contact_us"><h2>CONTACT</h2></Link></li>
@@ -150,7 +150,7 @@ const handleInquirySubmit = async (e) => {
       {isMenuOpen && (
         <div className={`${styles_1.mobile_navigation_menu} ${styles_1.fadeInDown}`}>
           <Link href="/"><h2 id={styles_1.active}>HOME</h2></Link>
-          <Link href="/packages"><h2>PACKAGES</h2></Link>
+          <Link href="/tour-packages"><h2>PACKAGES</h2></Link>
           <Link href="/services"><h2>SERVICES</h2></Link>
           <Link href="/destinations"><h2>DESTINATIONS</h2></Link>
           <Link href="/contact_us"><h2>CONTACT</h2></Link>
