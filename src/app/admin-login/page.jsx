41c95887e6fa661fe326ab05eda6e4a5cfb81082
Yohan Adikari <amkyadikari@gmail.com>
2026-04-07 21:06:35 +0530

    Add dynamic export to admin login and dashboard pages

--- src/app/admin-login/page.jsx
+++ src/app/admin-login/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = 'force-dynamic';
 import React, { useState, useEffect } from "react";
 import styles from "./admin_login.module.css";
 import Link from "next/link";
